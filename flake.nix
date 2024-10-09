280e3f2508a8aaccbb8b781fc4605c6e3d7df7ad
shyguy1412 <nilsramstoeck@gmail.com>
2024-10-09 09:12:23 +0200

    remove standalone home config

--- flake.nix
+++ flake.nix
@@ -29,18 +29,6 @@
           ];
         };
       };
-
-      # Standalone home-manager configuration entrypoint
-      # Available through 'home-manager --flake .#your-username@your-hostname'
-      homeConfigurations = {
-        "shy@dingus" = home-manager.lib.homeManagerConfiguration {
-          pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
-          extraSpecialArgs = { inherit inputs outputs; };
-          # > Our main home-manager configuration file <
-          modules = [ ./home-manager/home.nix ];
-        };
-      };
-
     };
 
 }
