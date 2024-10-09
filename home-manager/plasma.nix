e05320d9864139e8467e4ff91df098da24320380
shyguy1412 <nilsramstoeck@gmail.com>
2024-10-09 09:24:24 +0200

    remove unused plasma config

--- home-manager/plasma.nix
+++ home-manager/plasma.nix
@@ -2,25 +2,4 @@
 {
   imports = [
   ];
-
-  programs = {
-    plasma = {
-      enable = true;
-
-      workspace = {
-        # clickItemTo = "select";
-        # lookAndFeel = "org.kde.breezedark.desktop";
-        # cursor.theme = "Bibata-Modern-Ice";
-        # iconTheme = "Papirus-Dark";
-        # wallpaper = "${pkgs.kdePackages.plasma-workspace-wallpapers}/share/wallpapers/Patak/contents/images/1080x1920.png";
-      };
-
-      hotkeys.commands."launch-konsole" = {
-        name = "Launch Konsole";
-        key = "Meta+Alt+K";
-        command = "konsole";
-      };
-
-    };
-  };
 }
